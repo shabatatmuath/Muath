2143a231796fa7f4087a55a19bcf35bbe31adcee
muathshabatat <shabatatmuath@gmail.com>
2019-10-07 15:15:40 +0300

    edit

---
+++ java.js
@@ -0,0 +1,76 @@
+
+
+'use strict';
+var car = prompt('Am I a crazy driver?');
+car = car.toLowerCase();
+switch(car) {
+    case 'yes':
+       alert ('of course I\'m');
+       break;
+       case 'y':
+          alert ('of courseI\'m');
+          break;
+          case 'no':
+                alert ('I\'m a crazy driver actually');
+                break
+              case 'n':
+                  alert ('I\'m a crazy driver actually');
+                  break;
+          default:
+             alert ('wrong !');
+}
+
+var love = prompt('Do I love wake up early?');
+love = love.toLowerCase();
+if (love == 'yes') { 
+ alert('Actually i don\'t');
+} else if (love == 'y') {
+    alert('Actually i don\'t');
+} else if (love == 'no'|| love == 'NO'|| love == 'No' ) {
+     alert('right.. you\'re my friend')
+}
+else {
+    alert('wrong');
+}
+
+
+var live = prompt('Do I love living in the capital?');
+live = live.toLowerCase();
+if (live == 'yes') { 
+ alert('No I don\'t');
+} else if (live == 'y') {
+    alert('No I don\'t');
+} else if (live == 'no') {
+     alert('you\'re right')
+}
+else {
+    alert('wrong');
+}
+
+var meal = prompt('Is Manasaf the best meal for me ?');
+meal = meal.toLowerCase();
+if (meal == 'yes') { 
+ alert('who is the one does\'nt love Mansaf!');
+} else if (meal == 'y') {
+    alert('who is the one does\'nt love mansaf!');
+} else if (meal == 'no') {
+     alert('wrong awnser')
+}
+else {
+    alert('wrong');
+}
+
+
+
+var love = prompt('Do I love playing games?');
+love = love.toLowerCase();
+if (love == 'yes') { 
+ alert('unfortuntly I don\'t');
+} else if (love == 'y') {
+    alert('unfortuntly I don\'t');
+} else if (love == 'no') {
+     alert ('tha\'s true');
+}
+else {
+    alert('wrong');
+}
